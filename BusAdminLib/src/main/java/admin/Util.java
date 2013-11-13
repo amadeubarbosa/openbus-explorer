ca2bfc78f234ca7a72e721991cec4879e631a537
Tadeu Martins Chamuinho Bastos <tmartins@tecgraf.puc-rio.br>
2013-11-13 15:48:33 +0000

    [OPENBUS-2241] Criação da ferramenta BusExplorer - Melhoria na documentação da classe admin.Util da BusAdminLib
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/utils/busexplorer/trunk@146962 ae0415b3-e90b-0410-900d-d0be9363c56b

--- BusAdminLib/src/main/java/admin/Util.java
+++ BusAdminLib/src/main/java/admin/Util.java
@@ -17,26 +17,94 @@ import tecgraf.openbus.core.v2_0.services.access_control.NoLoginCode;
  */
 public class Util {
 
+  // TODO: [tmartins] Localização (idioma) da classe. 
+
+  /**
+   * Mensagem em caso de exceção TRANSIENT.
+   * 
+   * @see org.omg.CORBA.TRANSIENT
+   */
   static final String TRANSIENT_EXCEPTION_MESSAGE =
     "O barramento está inacessível no momento";
+
+  /**
+   * Mensagem em caso de exceção COMM_FAILURE.
+   * 
+   * @see org.omg.CORBA.COMM_FAILURE
+   */
   static final String COMM_FAILURE_EXCEPTION_MESSAGE =
     "Falha de comunicação ao acessar os serviços núcleo do barramento";
+
+  /**
+   * Mensagem em caso de exceção NO_PERMISSION com minor code NoLoginCode.
+   * 
+   * @see org.omg.CORBA.NO_PERMISSION
+   * @see tecgraf.openbus.core.v2_0.services.access_control.NoLoginCode
+   */
   static final String NO_LOGIN_EXCEPTION_MESSAGE =
     "Não há um login de válido no momento";
+
+  /**
+   * Mensagem em caso de exceção UnauthorizedOperation.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.UnauthorizedOperation
+   */
   public static final String UNAUTHORIZED_OPERATION_EXCEPTION_MESSAGE =
     "Operação não autorizada";
+
+  /**
+   * Mensagem em caso de exceção InvalidInterface.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.InvalidInterface
+   */
   public static final String INVALID_INTERFACE_EXCEPTION_MESSAGE =
     "Nome da interface inválido";
+
+  /**
+   * Mensagem em caso de exceção EntityAlreadyRegistered
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.EntityAlreadyRegistered
+   */
   public static final String ENTITY_ALREADY_REGISTERED_EXCEPTION_MESSAGE =
     "Entidade já registrada no barramento";
+
+  /**
+   * Mensagem em caso de exceção EntityCategoryAlreadyExists.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.EntityCategoryAlreadyExists
+   */
   public static final String ENTITY_CATEGORY_ALREADY_EXISTS_EXCEPTION_MESSAGE =
     "Categoria já existente no barramento";
+
+  /**
+   * Mensagem em caso exceção EntityCategoryInUse.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.EntityCategoryInUse
+   */
   public static final String ENTITY_CATEGORY_IN_USE_EXCEPTION_MESSAGE =
     "Categoria em uso por uma ou mais entidades";
+
+  /**
+   * Mensagem em caso de exceção InterfaceInUse.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.InterfaceInUse
+   */
   public static final String INTERFACE_IN_USE_EXCEPTION_MESSAGE =
     "Interface em uso por uma ou mais entidades";
+
+  /**
+   * Mensagem em caso de exceção AuthorizationInUse.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.offer_registry.admin.v1_0.AuthorizationInUse
+   */
   public static final String AUTHORIZATION_IN_USE_EXCEPTION_MESSAGE =
     "Autorização em uso por uma ou mais ofertas";
+
+  /**
+   * Mensagem em caso de exceção InvalidCertificate.
+   * 
+   * @see tecgraf.openbus.core.v2_0.services.access_control.admin.v1_0.InvalidCertificate
+   */
   public static final String INVALID_CERTIFICATE_EXCEPTION_MESSAGE =
     "Certificado inválido";
 

2ff2a76b9d8d8897bb0d8e038779a93aa031497e
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2017-07-17 16:40:53 -0300

    OPENBUS-3088 Nova ação para exportar integrações para planilha
    - exportação de outra planilha para visão de contratos do mapa

--- BusExplorer/src/main/java/busexplorer/panel/integrations/IntegrationExportToXLSAction.java
+++ BusExplorer/src/main/java/busexplorer/panel/integrations/IntegrationExportToXLSAction.java
@@ -12,6 +12,8 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import tecgraf.openbus.services.governance.v1_0.Consumer;
+import tecgraf.openbus.services.governance.v1_0.Contract;
 import tecgraf.openbus.services.governance.v1_0.Provider;
 
 import javax.swing.JFileChooser;
@@ -23,6 +25,8 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapper> {
 
@@ -38,6 +42,8 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
   @Override
   public void actionPerformed(ActionEvent actionEvent) {
     Workbook wb = new XSSFWorkbook();
+    Sheet adjacencySheet = wb.createSheet("adjacency matrix");
+    adjacencySheet.createRow(0);
     Sheet integrationsSheet = wb.createSheet("integrations");
     Row label = integrationsSheet.createRow(0);
     String[] columns = new String[]{"system-name", "contract", "manager-office", "provider", "type"};
@@ -47,6 +53,8 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
     }
     // to mark providers already listed as integrations
     List<String> visited = new ArrayList<String>();
+    // all labels necessary for adjacency matrix
+    Vector<String> matrix = new Vector<String>();
 
     BusExplorerTask<Void> task =
       new BusExplorerTask<Void>(ExceptionContext.Service) {
@@ -54,8 +62,29 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
         @Override
         protected void doPerformTask() throws Exception {
           List<Provider> providers = Application.login().extension.getProviders();
+          List<Consumer> consumers = Application.login().extension.getConsumers();
+          List<Contract> contracts = Application.login().extension.getContracts();
+          matrix.addAll(providers.parallelStream().map(Provider::name).collect(Collectors.toList()));
+          matrix.addAll(consumers.parallelStream().map(Consumer::name).collect(Collectors.toList()));
+          matrix.addAll(contracts.parallelStream().map(Contract::name).collect(Collectors.toList()));
+
+          for (int i = 0; i < matrix.size(); i++) {
+            adjacencySheet.getRow(0).createCell(i+1).setCellValue(matrix.get(i));
+            adjacencySheet.createRow(i+1).createCell(0).setCellValue(matrix.get(i));
+            for (int j = 0; j < matrix.size(); j++) {
+              adjacencySheet.getRow(i+1).createCell(j + 1).setCellValue(0);
+            }
+          }
+
           Application.login().extension.getIntegrations().forEach(integration -> {
             Arrays.stream(integration.contracts()).forEach(contract -> {
+              // adjacency sheet
+              int consumerIndex = matrix.indexOf(integration.consumer().name());
+              int providerIndex = matrix.indexOf(integration.provider().name());
+              int contractIndex = matrix.indexOf(contract.name());
+              adjacencySheet.getRow(consumerIndex+1).getCell(contractIndex+1).setCellValue(1);
+              adjacencySheet.getRow(contractIndex+1).getCell(providerIndex+1).setCellValue(1);
+              // integrations sheet
               Row row = integrationsSheet.createRow(integrationsSheet.getLastRowNum()+1);
               visited.add(integration.consumer().name());
               row.createCell(0).setCellValue(integration.consumer().name());
@@ -70,6 +99,13 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
             });
           });
           providers.forEach(provider -> {
+            // adjacency sheet
+            Arrays.stream(provider.contracts()).forEach(contract -> {
+              int contractIndex = matrix.indexOf(contract.name());
+              int providerIndex = matrix.indexOf(provider.name());
+              adjacencySheet.getRow(contractIndex+1).getCell(providerIndex+1).setCellValue(1);
+            });
+            // integrations sheet
             if (!visited.contains(provider.name())) {
               Row row = integrationsSheet.createRow(integrationsSheet.getLastRowNum()+1);
               row.createCell(0).setCellValue(provider.name());
@@ -79,6 +115,9 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
           });
           for (int c = 0; c < columns.length; c++) {
             integrationsSheet.autoSizeColumn(c);
+          }
+          for (int c = 0; c < matrix.size(); c++) {
+            adjacencySheet.autoSizeColumn(c);
           }
         }
 

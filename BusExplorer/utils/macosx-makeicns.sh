952a1875e1f7c203110ee2b0f63310bf8073daca
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2017-08-18 11:20:31 -0300

    OPENBUS-3092 Ícone alternativo 16x16

--- BusExplorer/utils/macosx-makeicns.sh
+++ BusExplorer/utils/macosx-makeicns.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 export PROJECT=BusExplorer
 export ICONDIR=$PROJECT.iconset
-if [ -z $1 ]; then echo -e "Usage: $0 <image.png>\n\timage.png must be 1024x1024 pixels at least\n"; exit 1; fi;
+
+if [ -z $1 ]; then 
+  echo -e "Usage: $0 <image.png> [image16x16.png]
+\t image.png      - must be 1024x1024 pixels at least
+\t image16x16.png - to use a different artwork for this very small icon\n"
+  exit 1
+fi
+if [ -n $2 ]; then ALTERNATIVE_16_ORIGICON=$2; fi;
 
 export ORIGICON=$1
 
 mkdir -p $ICONDIR
 
 # Normal screen icons
-for SIZE in 16 32 64 128 256 512; do
+if [ -n "$ALTERNATIVE_16_ORIGICON" ]; then
+  cp -f $ALTERNATIVE_16_ORIGICON $ICONDIR/icon_16x16.png
+else
+  SIZE=16
+  sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png ;
+fi
+
+for SIZE in 32 64 128 256 512; do
   sips -z $SIZE $SIZE $ORIGICON --out $ICONDIR/icon_${SIZE}x${SIZE}.png ;
 done
 

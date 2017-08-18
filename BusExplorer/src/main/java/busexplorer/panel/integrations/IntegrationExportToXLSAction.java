e23af5a414117637bf57f1184b9041aaebe20609
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2017-08-18 20:16:08 -0300

    OPENBUS-3088 Exportação de um novo sheet para a tabela de contatos
    - Uso do estilo de células para definir negrito nas linhas de cabeçalho
    - Strings traduzidas pelo arquivo de idiomas

--- BusExplorer/src/main/java/busexplorer/panel/integrations/IntegrationExportToXLSAction.java
+++ BusExplorer/src/main/java/busexplorer/panel/integrations/IntegrationExportToXLSAction.java
@@ -10,9 +10,12 @@ import busexplorer.panel.OpenBusAction;
 import busexplorer.utils.BusExplorerTask;
 import busexplorer.utils.Language;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import tecgraf.openbus.services.governance.v1_0.Consumer;
 import tecgraf.openbus.services.governance.v1_0.Contract;
@@ -49,16 +52,51 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
 
   @Override
   public void actionPerformed(ActionEvent actionEvent) {
-    Workbook wb = new XSSFWorkbook();
-    Sheet adjacencySheet = wb.createSheet("adjacency matrix");
+    XSSFWorkbook wb = new XSSFWorkbook();
+    XSSFFont headerFont = wb.createFont();
+    headerFont.setBold(true);
+    headerFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+    XSSFCellStyle headStyle = wb.createCellStyle();
+    headStyle.setFont(headerFont);
+    headStyle.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+    headStyle.setAlignment(CellStyle.ALIGN_CENTER);
+
+    Sheet integrationsSheet = wb.createSheet(getString("sheet.name.integration"));
+    Sheet adjacencySheet = wb.createSheet(getString("sheet.name.adjacency"));
+    Sheet contactsSheet = wb.createSheet(getString("sheet.name.contacts"));
+
+    // adjacency
     adjacencySheet.createRow(0);
-    Sheet integrationsSheet = wb.createSheet("integrations");
-    Row label = integrationsSheet.createRow(0);
-    String[] columns = new String[]{"system-name", "contract", "manager-office", "provider", "type"};
-    for (int c = 0; c < columns.length; c++) {
-      Cell cell = label.createCell(c);
-      cell.setCellValue(columns[c]);
+    // contacts
+    Row contactsSheetHeader = contactsSheet.createRow(0);
+    String[] contactsColumns = new String[]{
+      getString("sheet.contacts.system"),
+      getString("sheet.contacts.system.appcode"),
+      getString("sheet.contacts.system.manageroffice"),
+      getString("sheet.contacts.system.manager"),
+      getString("sheet.contacts.system.supportoffice"),
+      getString("sheet.contacts.system.support")
+    };
+    for (int c = 0; c < contactsColumns.length; c++) {
+      Cell cell = contactsSheetHeader.createCell(c);
+      cell.setCellValue(contactsColumns[c]);
+      cell.setCellStyle(headStyle);
     }
+    // integrations
+    Row integrationsSheetHeader = integrationsSheet.createRow(0);
+    String[] columns = new String[]{
+      getString("sheet.integration.system"),
+      getString("sheet.integration.system.manageroffice"),
+      getString("sheet.integration.contract"),
+      getString("sheet.integration.provider"),
+      getString("sheet.integration.type")
+    };
+    for (int c = 0; c < columns.length; c++) {
+      Cell cell = integrationsSheetHeader.createCell(c);
+      cell.setCellValue(columns[c]);
+      cell.setCellStyle(headStyle);
+    }
+
     // to mark providers already listed as integrations
     List<String> visited = new ArrayList<String>();
     // all labels necessary for adjacency matrix
@@ -77,8 +115,12 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
           matrix.addAll(contracts.parallelStream().map(Contract::name).collect(Collectors.toList()));
 
           for (int i = 0; i < matrix.size(); i++) {
-            adjacencySheet.getRow(0).createCell(i+1).setCellValue(matrix.get(i));
-            adjacencySheet.createRow(i+1).createCell(0).setCellValue(matrix.get(i));
+            Cell hcell = adjacencySheet.getRow(0).createCell(i+1);
+            hcell.setCellValue(matrix.get(i));
+            hcell.setCellStyle(headStyle);
+            Cell vcell = adjacencySheet.createRow(i+1).createCell(0);
+            vcell.setCellValue(matrix.get(i));
+            vcell.setCellStyle(headStyle);
             for (int j = 0; j < matrix.size(); j++) {
               adjacencySheet.getRow(i+1).createCell(j + 1).setCellValue(0);
             }
@@ -96,31 +138,54 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
               Row row = integrationsSheet.createRow(integrationsSheet.getLastRowNum()+1);
               visited.add(integration.consumer().name());
               row.createCell(0).setCellValue(integration.consumer().name());
-              row.createCell(1).setCellValue(contract.name());
-              row.createCell(2).setCellValue(integration.consumer().manageroffice());
+              row.createCell(1).setCellValue(integration.consumer().manageroffice());
+              row.createCell(2).setCellValue(contract.name());
               row.createCell(3).setCellValue(integration.provider().name());
               if (providers.stream().anyMatch(provider -> provider.name().equals(integration.consumer().name()))) {
-                row.createCell(4).setCellValue("Consumer/Provider");
+                row.createCell(4).setCellValue(getString("sheet.integration.type.both"));
               } else {
-                row.createCell(4).setCellValue("Consumer");
+                row.createCell(4).setCellValue(getString("sheet.integration.type.onlyconsumer"));
               }
             });
           });
+          consumers.forEach(consumer -> {
+              String consumerName = consumer.name();
+              // contacts sheet
+              Row contactRow = contactsSheet.createRow(matrix.indexOf(consumerName) + 1);
+              contactRow.createCell(0).setCellValue(consumerName);
+              contactRow.createCell(1).setCellValue(consumer.code());
+              contactRow.createCell(2).setCellValue(consumer.manageroffice());
+              contactRow.createCell(3).setCellValue(String.join(",", consumer.manager()));
+              contactRow.createCell(4).setCellValue(consumer.supportoffice());
+              contactRow.createCell(5).setCellValue(String.join(",", consumer.support()));
+          });
           providers.forEach(provider -> {
+            String providerName = provider.name();
+            // contacts sheet
+            Row contactRow = contactsSheet.createRow(matrix.indexOf(providerName)+1);
+            contactRow.createCell(0).setCellValue(providerName);
+            contactRow.createCell(1).setCellValue(provider.code());
+            contactRow.createCell(2).setCellValue(provider.manageroffice());
+            contactRow.createCell(3).setCellValue(String.join(",", provider.manager()));
+            contactRow.createCell(4).setCellValue(provider.supportoffice());
+            contactRow.createCell(5).setCellValue(String.join(",", provider.support()));
             // adjacency sheet
             Arrays.stream(provider.contracts()).forEach(contract -> {
               int contractIndex = matrix.indexOf(contract.name());
-              int providerIndex = matrix.indexOf(provider.name());
+              int providerIndex = matrix.indexOf(providerName);
               adjacencySheet.getRow(contractIndex+1).getCell(providerIndex+1).setCellValue(1);
             });
             // integrations sheet
-            if (!visited.contains(provider.name())) {
+            if (!visited.contains(providerName)) {
               Row row = integrationsSheet.createRow(integrationsSheet.getLastRowNum()+1);
-              row.createCell(0).setCellValue(provider.name());
-              row.createCell(2).setCellValue(provider.manageroffice());
-              row.createCell(4).setCellValue("Provider");
+              row.createCell(0).setCellValue(providerName);
+              row.createCell(1).setCellValue(provider.manageroffice());
+              row.createCell(4).setCellValue(getString("sheet.integration.type.onlyprovider"));
             }
           });
+          for (int c = 0; c < contactsColumns.length; c++) {
+            contactsSheet.autoSizeColumn(c);
+          }
           for (int c = 0; c < columns.length; c++) {
             integrationsSheet.autoSizeColumn(c);
           }
@@ -149,7 +214,7 @@ public class IntegrationExportToXLSAction extends OpenBusAction<IntegrationWrapp
                 super.approveSelection();
               }
             };
-            chooser.setFileFilter(new FileNameExtensionFilter("Microsoft Excel Workbook / Open XML Spreadsheet", "xlsx"));
+            chooser.setFileFilter(new FileNameExtensionFilter(getString("export.extensionfilter.name"), "xlsx"));
             chooser.setAcceptAllFileFilterUsed(false);
             chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
             int status = chooser.showSaveDialog(this.parentWindow);

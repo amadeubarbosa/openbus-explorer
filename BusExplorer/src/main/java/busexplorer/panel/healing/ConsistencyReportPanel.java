58e44701554661685e1c75f31234b75bf99c0c94
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2017-10-05 16:54:04 -0300

    OPENBUS-3099 Painéis das pendências passam a ser colapsáveis
    - Incorporando uso da biblioteca Japura para ter painéis colapsáveis
    - Uso do JScrollPane ficou melhor com os painéis colapsáveis
    - CardLayout agora possui 3 cards (gif carregando, label okay, painéis colapsáveis)

--- BusExplorer/src/main/java/busexplorer/panel/healing/ConsistencyReportPanel.java
+++ BusExplorer/src/main/java/busexplorer/panel/healing/ConsistencyReportPanel.java
@@ -23,25 +23,74 @@ import busexplorer.panel.TablePanelComponent;
 import busexplorer.utils.BusExplorerTask;
 import busexplorer.utils.Language;
 import net.miginfocom.swing.MigLayout;
+import org.japura.gui.CollapsiblePanel;
+import org.japura.gui.CollapsibleRootPanel;
 
 public class ConsistencyReportPanel extends RefreshablePanel {
 
   private final JPanel cards;
-  private final LinkedHashMap<JLabel, TablePanelComponent> uiComponents;
+  private final CollapsibleRootPanel collapsiblePane;
+  private final LinkedHashMap<String, TablePanelComponent> uiComponents;
   private final JFrame parentWindow;
-  private final JPanel loadingPane;
-  private final JPanel dataPane;
 
   public ConsistencyReportPanel(JFrame parentWindow) {
     super(new BorderLayout());
     this.parentWindow = parentWindow;
-    JPanel header = new JPanel(new MigLayout("fill, flowy"));
 
+    JPanel header = new JPanel(new MigLayout("fill, flowy"));
     JLabel headingLabel = new JLabel(ApplicationIcons.ICON_HEALTHY_32);
     headingLabel.setText(getString("title"));
     header.add(headingLabel, "growx, push");
     header.add(new JSeparator(JSeparator.HORIZONTAL), "grow");
+    this.add(header, BorderLayout.NORTH);
 
+    this.uiComponents = new LinkedHashMap<>();
+    this.uiComponents.put(getString("label.integration.missing.basic"),
+      new IntegrationMissingBasicInformation(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.consumer.missing.basic"),
+      new ConsumerMissingBasicInformation(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.provider.missing.basic"),
+      new ProviderMissingBasicInformation(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.provider.missing.contracts"),
+      new ProviderMissingContracts(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.provider.missing.busquery"),
+      new ProviderMissingBusQuery(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.provider.missing.authorizations"),
+      new ProviderMissingAuthorizations(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.authorization.missing.provider"),
+      new AuthorizationMissingProvider(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.authorization.missing.offer"),
+      new AuthorizationMissingOffer(parentWindow).buildTableComponent());
+    this.uiComponents.put(getString("label.offer.missing.provider"),
+      new OfferMissingProvider(parentWindow).buildTableComponent());
+
+    ImageIcon imageIcon = ApplicationIcons.ICON_LOADING_32;
+    JLabel iconLabel = new JLabel(imageIcon);
+    imageIcon.setImageObserver(iconLabel);
+    JPanel loadingPane = new JPanel();
+    loadingPane.add(iconLabel);
+
+    JLabel okayPane = new JLabel(getString("label.everything.okay"));
+    okayPane.setIcon(ApplicationIcons.ICON_VALIDATE_16);
+    okayPane.setHorizontalAlignment(JLabel.CENTER);
+
+    this.collapsiblePane = new CollapsibleRootPanel(CollapsibleRootPanel.FILL);
+    this.collapsiblePane.setBackground(null);
+    JScrollPane scrollPane = new JScrollPane(this.collapsiblePane);
+    scrollPane.setMaximumSize(this.getSize());
+    scrollPane.setViewportBorder(null);
+    scrollPane.setBorder(null);
+    scrollPane.getInsets().set(0,0,0,0);
+
+    this.cards = new JPanel(new CardLayout());
+    this.cards.add(loadingPane);
+    this.cards.add(okayPane);
+    this.cards.add(scrollPane);
+    this.add(cards, BorderLayout.CENTER);
+
+    JPanel footer = new JPanel(new MigLayout("fill, flowy"));
+    footer.add(new JSeparator(JSeparator.HORIZONTAL), "grow");
+    JPanel refreshPanel = new JPanel(new MigLayout("align center"));
     JButton refreshButton = new JButton();
     refreshButton.setAction(new AbstractAction() {
       @Override
@@ -50,57 +99,19 @@ public class ConsistencyReportPanel extends RefreshablePanel {
       }
     });
     refreshButton.setText(Language.get(TablePanelComponent.class, "refresh"));
+    refreshButton.setMnemonic(refreshButton.getText().charAt(0));
     refreshButton.setToolTipText(Language.get(TablePanelComponent.class,
       "refresh.tooltip"));
     refreshButton.setIcon(ApplicationIcons.ICON_REFRESH_16);
-    this.add(header, BorderLayout.NORTH);
-
-    this.uiComponents = new LinkedHashMap<>();
-    this.uiComponents.put(new JLabel(getString("label.integration.missing.basic")),
-      new IntegrationMissingBasicInformation(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.consumer.missing.basic")),
-      new ConsumerMissingBasicInformation(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.provider.missing.basic")),
-      new ProviderMissingBasicInformation(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.provider.missing.contracts")),
-      new ProviderMissingContracts(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.provider.missing.busquery")),
-      new ProviderMissingBusQuery(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.provider.missing.authorizations")),
-      new ProviderMissingAuthorizations(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.authorization.missing.provider")),
-      new AuthorizationMissingProvider(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.authorization.missing.offer")),
-      new AuthorizationMissingOffer(parentWindow).buildTableComponent());
-    this.uiComponents.put(new JLabel(getString("label.offer.missing.provider")),
-      new OfferMissingProvider(parentWindow).buildTableComponent());
-
-    ImageIcon imageIcon = ApplicationIcons.ICON_LOADING_32;
-    JLabel iconLabel = new JLabel(imageIcon);
-    imageIcon.setImageObserver(iconLabel);
-    this.loadingPane = new JPanel();
-    this.loadingPane.add(iconLabel);
-
-    this.dataPane = new JPanel(new MigLayout("fill, flowy"));
-
-    this.cards = new JPanel(new CardLayout());
-    this.cards.add(loadingPane);
-    JScrollPane scroll = new JScrollPane(dataPane);
-    scroll.setMaximumSize(this.getSize());
-    scroll.setViewportBorder(null);
-    scroll.setBorder(null);
-    this.cards.add(scroll);
-
-    this.add(cards, BorderLayout.CENTER);
-    JPanel refreshPane = new JPanel(new MigLayout("align center"));
-    refreshPane.add(refreshButton);
-    this.add(refreshPane, BorderLayout.SOUTH);
+    refreshPanel.add(refreshButton);
+    footer.add(refreshPanel, "grow, push, pad 0");
+    this.add(footer, BorderLayout.SOUTH);
   }
 
   @Override
   public void refresh(ActionEvent event) {
     CardLayout cardLayoutManager = ((CardLayout) this.cards.getLayout());
-    Set<Map.Entry<JLabel, TablePanelComponent>> uiElements = this.uiComponents.entrySet();
+    Set<Map.Entry<String, TablePanelComponent>> uiElements = this.uiComponents.entrySet();
     cardLayoutManager.first(this.cards);
     new BusExplorerTask<Void>(ExceptionContext.Service) {
       @Override
@@ -111,23 +122,22 @@ public class ConsistencyReportPanel extends RefreshablePanel {
       @Override
       protected void afterTaskUI() {
         if (getStatus()) {
-          ConsistencyReportPanel.this.dataPane.removeAll();
+          collapsiblePane.removeAll();
           uiElements.stream().forEach(entry -> {
-            JLabel labelComponent = entry.getKey();
             TablePanelComponent<?> tableComponent = entry.getValue();
             tableComponent.setPreferredSize(new Dimension(400, 100));
             if (tableComponent.getElements().isEmpty() == false) {
-              ConsistencyReportPanel.this.dataPane.add(labelComponent, "grow, pad 0 10 0 -10");
-              ConsistencyReportPanel.this.dataPane.add(tableComponent, "grow, push, pad 0 10 0 -10");
+              CollapsiblePanel collapsible = new CollapsiblePanel(entry.getKey());
+              collapsible.getInsets().set(0, 10, 0, 10);
+              collapsible.add(tableComponent, "grow");
+              collapsiblePane.add(collapsible);
             }
           });
-          if (ConsistencyReportPanel.this.dataPane.getComponents().length == 0) {
-            JLabel okay = new JLabel(getString("label.everything.okay"));
-            okay.setIcon(ApplicationIcons.ICON_VALIDATE_16);
-            okay.setHorizontalAlignment(JLabel.CENTER);
-            ConsistencyReportPanel.this.dataPane.add(okay, "grow");
+          if (collapsiblePane.getComponents().length == 0) {
+            cardLayoutManager.next(ConsistencyReportPanel.this.cards);
+          } else {
+            cardLayoutManager.last(ConsistencyReportPanel.this.cards);
           }
-          cardLayoutManager.last(ConsistencyReportPanel.this.cards);
         }
       }
     }.execute(this.parentWindow, getString("waiting.title"), getString("waiting.msg"), 2, 0);
